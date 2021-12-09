1d10b42de9826f4b7b79c43165afd25b7d0ccb36
fheyen <frank.heyen@outlook.de>
2021-12-09 22:17:24 +0100

    + minor responsiveness improvements

--- scripts/image-resizer.js
+++ scripts/image-resizer.js
@@ -2,6 +2,15 @@ import { readdirSync } from 'fs';
 import path from 'path';
 import { Image } from 'image-js';
 
+
+// TODO: for better quality, maybe implement a low level interpolation based on https://github.com/hughsk/bicubic
+// TODO: or use opencv.js?
+// https://docs.opencv.org/3.4/dc/de6/tutorial_js_nodejs.html
+// https://stackoverflow.com/questions/62641640/image-resize-interpolation-with-pure-javascript
+
+
+
+
 if (process.argv.length !== 4) {
     console.error('No directory or targetWidth argument given, run program as "node image-resizer.js ./dir 64" for rescaling images in ./dir to 64px');
     console.log(process.argv);
