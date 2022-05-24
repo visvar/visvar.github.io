fc4b8d7d525aa83fcc66d17e501ab578f4585ece
fheyen <frank.heyen@outlook.de>
2022-05-24 23:08:00 +0200

    image-resizer now causes less file writes and does not enlarge images

--- scripts/image-resizer.js
+++ scripts/image-resizer.js
@@ -8,9 +8,9 @@ import { readSync, writeSync } from 'image-js'
 // https://github.com/image-js/image-js/issues/496#issuecomment-583483759
 
 if (process.argv.length !== 4) {
-    console.error('No directory or targetWidth argument given, run program as "node image-resizer.js ./dir 64" for rescaling images in ./dir to 64px')
-    console.log(process.argv)
-    process.exit(1)
+  console.error('No directory or targetWidth argument given, run program as "node image-resizer.js ./dir 64" for rescaling images in ./dir to 64px')
+  console.log(process.argv)
+  process.exit(1)
 }
 
 const directory = process.argv[2]
@@ -19,25 +19,52 @@ const targetWidth = +process.argv[3]
 rescale(directory, targetWidth)
 
 async function rescale (directory, targetWidth) {
-    console.log(`image-resizer.js, dir=${directory} width=${targetWidth}`)
-    const files = readdirSync(directory)
-    for (const file of files) {
-        if (!file.includes('.') || file.endsWith('.svg')) {
-            // Subdirectory or at least no image file
-            continue
-        }
-        console.log(file)
-        try {
-            const image = await readSync(path.join(directory, file))
-            const processed = image.resize({
-                width: targetWidth,
-                interpolation: 'BICUBIC',
-            })
-            const outPath = path.join(directory, 'small', file)
-            writeSync(outPath, processed)
-        } catch (error) {
-            console.log('Error:')
-            console.log(error.message)
-        }
+  console.log(`image-resizer.js, dir=${directory} width=${targetWidth}`)
+  const files = readdirSync(directory)
+  let noChange = 0
+  for (const file of files) {
+    if (!file.includes('.') || file.endsWith('.svg')) {
+      // Subdirectory or at least no image file
+      continue
     }
+    try {
+      const image = await readSync(path.join(directory, file))
+      let processed
+      if (image.width <= targetWidth) {
+        // Only make smaller, not larger
+        // console.log(`  image width ${image.width} <= target width ${targetWidth}, copying`)
+        processed = image
+      } else {
+        processed = image.resize({
+          width: targetWidth,
+          interpolation: 'BICUBIC',
+        })
+      }
+      // Compare to old one and do not overwrite if identical
+      const outPath = path.join(directory, 'small', file)
+      try {
+        const old = await readSync(outPath)
+        if (processed.data.length === old.data.length) {
+          let identical = true
+          for (let i = 0; i < processed.data.length; i++) {
+            if (processed.data[i] !== old.data[i]) {
+              identical = false
+              break
+            }
+          }
+          if (identical) {
+            // console.log("  no change, skipping")
+            noChange++
+            continue
+          }
+        }
+      } catch {
+        // Doesn't matter, just write the image in case checking for identical fails
+      }
+      writeSync(outPath, processed)
+    } catch (error) {
+      console.log(`Error with ${file}:`)
+      console.log(error.message)
+    }
+  }
 }
